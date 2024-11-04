12fc290b5eb7775f4b6667654109ce7ab1fef37c
isaacronquest <isaacronquest@gmail.com>
2024-11-04 20:39:06 +0200

    Enable button functionality and two number  calculations

--- script.js
+++ script.js
@@ -1,41 +1,105 @@
-let numA = 0;
-let numB = 0;
+let numA = '';
+let numB = '';
 let operator = '';
 
 function add (a,b) {
-    console.log(a + b);
-    // return a + b;
+    // console.log(a + b);
+    return a + b;
 }
 
 function subtract (a,b) {
-    console.log(a - b);
-    // return a - b;
+    // console.log(a - b);
+    return a - b;
 }
 
 function multiply (a,b) {
-    console.log(a * b);
-    // return a * b;
+    // console.log(a * b);
+    return a * b;
 }
 
 function divide(a,b) {
-    console.log(a / b);
-    // return a / b;
+    // console.log(a / b);
+    return a / b;
 }
 
-function operate(op,nA,nB) {
+function operate(op,a,b) {
+    nA = Number(a);
+    nB = Number(b);
     switch (op) {
         case '+':
-            add(nA,nB);
-            break;
+            return add(nA,nB);
         case '-':
-            subtract (nA,nB);
-            break;
-        case '*':
-            multiply(nA,nB);
-            break;
+            return subtract (nA,nB);
+        case 'x':
+            return multiply(nA,nB);
         case '/':
-            divide(nA,nB);
-            break;
+            return divide(nA,nB);
     }
 }
 
+const screen = document.querySelector('.displayDigits');
+
+const digitBtn = document.querySelectorAll('.digits');
+
+digitBtn.forEach(button => {
+    button.addEventListener('click', digitClickA);
+});
+
+function digitClickA(event) {
+    const buttonText = event.target.textContent;
+    numA += buttonText
+    screen.textContent = numA;
+}
+
+function digitClickB(event) {
+    const buttonText = event.target.textContent;
+    numB += buttonText
+    screen.textContent += numB;
+}
+
+
+
+const opBtn = document.querySelectorAll('.operator');
+
+opBtn.forEach(button => {
+    button.addEventListener('click', opFunc);
+});
+
+function opFunc (event) {
+    operator = event.target.textContent;
+    screen.textContent += operator;
+
+    digitBtn.forEach(button => {
+        button.removeEventListener('click', digitClickA)
+        button.addEventListener('click', digitClickB);
+    });
+};
+
+
+
+const equalBtn = document.querySelector('#equalBtn');
+
+equalBtn.addEventListener("click", () => {
+    let answer = '';
+
+    if (numA != '' && numB != '' && operator != '') {
+        answer = operate(operator,numA,numB);
+        screen.textContent = answer;
+    }
+});
+
+
+
+const clearBtn = document.querySelector('#clearBtn');
+
+clearBtn.addEventListener("click", () => {
+    numA = '';
+    numB = '';
+    operator = '';
+    screen.textContent = '0';
+
+    digitBtn.forEach(button => {
+        button.removeEventListener('click', digitClickB)
+        button.addEventListener('click', digitClickA);
+    });
+});
